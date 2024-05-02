482ecc02222096c9b54a13fe14b8aea28dacf3f5
Germán Castro <gecastro@gmail.com>
2024-05-02 16:43:50 +1000

    Add changes

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 # setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
+source "${ROS_WORKSPACE}/install/setup.bash" --
 
-#exec "$@"
+# exec "$@"
 
-ros2 run mavros mavros_node --ros-args -p fcu_url:=$FCU_URL -p gcs_url:=$GCS_URL
+# ros2 run mavros mavros_node --ros-args -p fcu_url:=$FCU_URL -p gcs_url:=$GCS_URL
+ros2 launch rover_bringup bringup_launch.py  fcu_url:=$FCU_URL  gcs_url:=$GCS_URL
