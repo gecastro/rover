1c47b28451abde4797627c8fbce40c7b62f3025e
Germán Castro <gecastro@gmail.com>
2024-05-03 17:44:09 +1000

    update setup

--- rover_bringup/setup.py
+++ rover_bringup/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/bringup_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/bringup_launch.py','launch/rs_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
